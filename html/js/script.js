abe80d3e223a006a120f872ecde23d5b94a2d97d
Rong Fan <30935707+rongwfan@users.noreply.github.com>
2018-10-09 12:02:11 -0500

    Uploaded html folder

---
+++ html/js/script.js
@@ -0,0 +1,68 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width='300px';
+}
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width='0px';
+}
+
+// img1 //
+function openSlideShow1(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img1.src;
+}
+
+function closeSlideShow(){
+    document.getElementById('gallery_modal').style.display='none';
+}
+
+// img2 //
+function openSlideShow2(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img2.src;
+}
+
+// img3 //
+function openSlideShow3(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img3.src;
+}
+
+// img4 //
+function openSlideShow4(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img4.src;
+}
+
+// img5 //
+function openSlideShow5(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img5.src;
+}
+
+// img6 //
+function openSlideShow6(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img6.src;
+}
+
+
+// img7 //
+function openSlideShow7(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img7.src;
+}
+
+
+// img8 //
+function openSlideShow8(){
+    document.getElementById('gallery_modal').style.display='block';
+    var img = document.getElementById('gallery_img');
+    img.src = img8.src;
+}
